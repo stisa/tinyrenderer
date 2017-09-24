5830c05e6ff077b44a8d2e1446d0ef216e65bd95
stisa <stisa4@gmail.com>
2017-09-24 20:33:47 +0200

    move line proc

--- tinyrenderer.nim
+++ tinyrenderer.nim
@@ -1,39 +1,5 @@
 import pnghelper,objparser,matutils
 
-proc line*(srf:var Image, x1,y1,x2,y2:int, color : Color = White) =
-  ## Draws a line between x1,y1 and x2,y2. Uses Bresenham's line algorithm.
-  #echo("x$1 y$2 x$3 y$4" % [$x1,$y1,$x2,$y2])
-  var steep :bool
-  var 
-    xx1 = x1
-    yy1 = y1
-    xx2 = x2
-    yy2 = y2
-  #echo "1 ", xx1
-  #echo "2 ",xx2
-  if abs(xx1-xx2)<abs(yy1-yy2):
-    swap(xx1,yy1)
-    swap(xx2,yy2)
-    steep = true
-  if xx1>xx2:
-    swap(xx1,xx2)
-    swap(yy1,yy2)
-  
-  let dx = xx2-xx1
-  let dy = yy2-yy1
-  let de2 = abs(dy)*2
-  var e2 = 0
-  var y = yy1
-  for x in xx1..<xx2:
-    if steep:
-      srf[y,x] = color
-    else:
-      srf[x,y] = color
-    e2 += de2
-    if e2>dx:
-      y = if yy2>yy1: y+1 else: y-1
-      e2 -= dx*2
-
 proc wireframe(srf:var Image,m:Objmodel) =
   var 
     v0,v1 :Vertex
